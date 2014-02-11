46f8a5c04207a4415dc915b296a5aec8500cf9e2
Willian van der Velde <willian@pythonheads.nl>
2014-02-11 11:32:12 +0100

    Added spec for product redirection

--- spec/controllers/spree/admin/scan_and_go_controller_spec.rb
+++ spec/controllers/spree/admin/scan_and_go_controller_spec.rb
@@ -5,6 +5,15 @@ describe Spree::Admin::ScanAndGoController do
   let(:order) { create :order }
 
   describe '.redirect' do
+    context 'a product id is given' do
+      let(:product) { create :product }
+
+      it 'redirects to the product page' do
+        spree_get :redirect, { scan_and_go_input: product.id }
+        expect(response).to redirect_to("/admin/products/#{product.permalink}/edit")
+      end
+    end 
+
     context 'a order number is given' do
       it 'redirects to the order page' do
         spree_get :redirect, { scan_and_go_input: order.number }
@@ -23,7 +32,6 @@ describe Spree::Admin::ScanAndGoController do
 
     context 'a email address is given' do
       let(:user) { create :user }
-
       it 'redirects to the users page' do
         spree_get :redirect, { scan_and_go_input: user.email }
         expect(response).to redirect_to("/admin/users/#{user.id}/edit")
